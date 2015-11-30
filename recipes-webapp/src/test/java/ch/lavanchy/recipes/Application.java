9325a8adf04927fe8e644fa008d29eb7e64e750d
Cedric Lavanchy <cedric.lavanchy.pro@gmail.com>
2015-11-30 18:18:08 +0100

    [cla] - Add the node/bower config

--- recipes-webapp/src/test/java/ch/lavanchy/recipes/Application.java
+++ recipes-webapp/src/test/java/ch/lavanchy/recipes/Application.java
@@ -2,6 +2,9 @@ package ch.lavanchy.recipes;
 
 import ch.lavanchy.recipes.config.ApplicationConfig;
 import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.server.handler.ContextHandler;
+import org.eclipse.jetty.server.handler.ResourceHandler;
+import org.eclipse.jetty.util.resource.Resource;
 import org.glassfish.jersey.jetty.JettyHttpContainerFactory;
 import org.glassfish.jersey.server.ResourceConfig;
 
@@ -15,9 +18,16 @@ import java.net.URI;
  */
 public class Application {
     public static void main(String[] args) throws Exception {
+        final ContextHandler context = new ContextHandler();
         final URI baseUri = UriBuilder.fromUri("http://localhost/").port(9998).build();
         final ResourceConfig config = new ResourceConfig(new ApplicationConfig());
+        final ResourceHandler resourceHandler = new ResourceHandler();
+        resourceHandler.setDirectoriesListed(true);
+        //resourceHandler.setWelcomeFiles(new String[]{ "index.html" });
+        resourceHandler.setBaseResource(Resource.newResource("."));
+        context.setHandler(resourceHandler);
         final Server jettyServer = JettyHttpContainerFactory.createServer(baseUri, config);
+        jettyServer.setHandler(context);
 
         try {
             jettyServer.start();
