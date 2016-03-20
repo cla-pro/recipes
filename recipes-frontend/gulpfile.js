ff21ece1eaee524791a784e580882bcb0aa58fdf
Cedric Lavanchy <cedric.lavanchy.pro@gmail.com>
2016-03-20 20:13:49 +0100

    [cla]
    - remove the icon borders
    - add the version in the footer
    - fix the header bar
    - update the cache manifest and move to version 1.0.2

--- recipes-frontend/gulpfile.js
+++ recipes-frontend/gulpfile.js
@@ -31,30 +31,30 @@ gulp.task('analyze', function () {
 
 gulp.task('build-dev', function() {
     gulp.src(['target/classes/images/*.png', 'target/classes/images/*.jpg', 'target/classes/images/*.gif'])
-            .pipe(gulp.dest('dist/images'));
+            .pipe(gulp.dest('target/dist/images'));
 
     gulp.src(['target/classes/partials/*.html'])
         .pipe($$.htmlhint({'doctype-first': false}))
         .pipe($$.htmlhint.reporter())
-        .pipe(gulp.dest('dist/partials'));
+        .pipe(gulp.dest('target/dist/partials'));
 
     gulp.src(['target/classes/bower_components/**/*'])
-        .pipe(gulp.dest('dist/bower_components'));
+        .pipe(gulp.dest('target/dist/bower_components'));
 
     gulp.src(['target/classes/js/**/*.js'])
-        .pipe(gulp.dest('dist/js'));
+        .pipe(gulp.dest('target/dist/js'));
 
     gulp.src(['target/classes/css/**/*.css'])
-        .pipe(gulp.dest('dist/css'));
+        .pipe(gulp.dest('target/dist/css'));
 
     return gulp.src('target/classes/index.html')
         .pipe($$.debug({title: 'Processed output File: '}))
-        .pipe(gulp.dest('dist'));
+        .pipe(gulp.dest('target/dist'));
 });
 
 gulp.task('release', function() {
     gulp.src(['target/classes/images/*.png', 'target/classes/images/*.jpg', 'target/classes/images/*.gif'])
-        .pipe(gulp.dest('dist/images'));
+        .pipe(gulp.dest('target/dist/images'));
     
     var partials = gulp.src(['target/classes/**/*.html', '!target/classes/index.html', '!target/classes/bower_components/**/*.html'])
         .pipe($$.htmlhint({'doctype-first': false}))
@@ -65,10 +65,10 @@ gulp.task('release', function() {
         }))
         .pipe($$.concat({path: 'template.js', cwd: ''}))
         //.pipe($$.rev())
-        .pipe(gulp.dest('dist'));
+        .pipe(gulp.dest('target/dist'));
 
     gulp.src(['target/classes/*.appcache'])
-        .pipe(gulp.dest('dist'));
+        .pipe(gulp.dest('target/dist'));
 
     var main = gulp.src('target/classes/*.html')
         .pipe($$.useref())
@@ -77,12 +77,12 @@ gulp.task('release', function() {
         .pipe(gulpif('*.css', $$.cssnano()))
         //.pipe(gulpif('*.css', $$.rev()))
         .pipe($$.revReplace())
-        .pipe(inject(partials, { ignorePath: ['dist/'], addRootSlash: false, starttag: '<!-- inject:template-js -->' }))
+        .pipe(inject(partials, { ignorePath: ['target/dist/'], addRootSlash: false, starttag: '<!-- inject:template-js -->' }))
         .pipe(gulpif('*.html', $$.htmlmin({ removeComments: true })));
     
     return eventStream.merge(partials, main)
         .pipe($$.debug({title: 'Processed output File: '}))
-        .pipe(gulp.dest('dist'));
+        .pipe(gulp.dest('target/dist'));
 });
 
 //gulp.task('default', ['analyze', 'release']); // Dependencies are run in parallel, so an error in analyze does not stop the build
