ba7d1a7e65ab957f6b3359c538e3a1fb46b3d2ed
Cedric Lavanchy <cedric.lavanchy.pro@gmail.com>
2016-08-05 19:07:40 +0200

    [cla] - reorganize the actions icons

--- recipes-frontend/app/shared/action/action.js
+++ recipes-frontend/app/shared/action/action.js
@@ -8,7 +8,20 @@
             templateUrl: 'shared/action/action.html',
             transclude: true,
             controllerAs: 'vm',
-            controller: ['$scope', '$stateParams', function($scope, $stateParams) {}],
+            controller: ['$scope', '$stateParams', function($scope, $stateParams) {
+                $scope.hasAdditionalActions = $scope.displayEdit || $scope.displayDownload;
+                $scope.displayAdditionalActions = false;
+
+                var that = $scope;
+                var updateHasAdditionalActions = function() {
+                    that.hasAdditionalActions = that.displayEdit || that.displayDownload;
+                };
+                $scope.$watch(function() { return that.displayEdit; }, updateHasAdditionalActions);
+                $scope.$watch(function() { return that.displayDownload; }, updateHasAdditionalActions);
+                $scope.clickAdditional = function() {
+                    $scope.displayAdditionalActions = !$scope.displayAdditionalActions;
+                };
+            }],
             scope: {
                 displayBack: '=',
                 displayEdit: '=',
